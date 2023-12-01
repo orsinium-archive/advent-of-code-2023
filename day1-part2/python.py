e86d14f90c8b28eb1560795b63626465c1ee2fc0
gram <git@orsinium.dev>
2023-12-01 13:14:37 +0100

    simplify

--- day1-part2/python.py
+++ day1-part2/python.py
@@ -1,37 +1,42 @@
 #!/usr/bin/env python3
 import sys
 import re
-from string import digits
-
-words = ('one', 'two', 'three', 'four', 'five',
-         'six', 'seven', 'eight', 'nine')
 
 
 def rev_str(line: str) -> str:
     return ''.join(reversed(line))
 
 
-words_rev = tuple(rev_str(w) for w in words)
+def get_replacement_map() -> dict[str, int]:
+    words = ('one', 'two', 'three', 'four', 'five',
+             'six', 'seven', 'eight', 'nine')
+    word_map = {w: i for i, w in enumerate(words, start=1)}
+    for i in range(1, 10):
+        word_map[str(i)] = i
+    for i, w in enumerate(words, start=1):
+        word_map[rev_str(w)] = i
+    return word_map
+
+
+# The map looks like this:
+#   {'one': 1, 'two': 2, ..., '1': 1, '2': 2, ..., 'eno': 1, 'owt': 2, ...}
+word_map = get_replacement_map()
 
 
 def replace_word(match: re.Match[str]) -> str:
-    word = match.group(0)
-    if len(word) == 1:
-        return word
-    return str(words.index(word) + 1)
+    return str(word_map[match.group(0)])
 
 
-def replace_word_rev(word: re.Match[str]) -> str:
-    return str(words_rev.index(word.group(0)) + 1)
-
-
-rex = re.compile('|'.join(words) + '|' + '|'.join(digits))
-rex_rev = re.compile('|'.join(words_rev))
+rex = re.compile('|'.join(word_map))
 
 result = 0
 for line1 in sys.stdin:
+    # replace the first match
     line = rex.sub(replace_word, line1, 1)
-    line = rev_str(rex_rev.sub(replace_word_rev, rev_str(line), 1))
+    # replace the last match in a reversed string and reverse it back
+    line = rev_str(rex.sub(replace_word, rev_str(line), 1))
+    # filter out digits only
     line = ''.join(c for c in line if c.isdigit())
+    # take the first and the last digit
     result += int(line[0] + line[-1])
 print(result)
